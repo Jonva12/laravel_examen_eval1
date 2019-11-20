fa37290dbe17c5604d91c091d7e76e5fca07ed33
“ajuanenama” <ajuanenama@zubirimanteo.com>
2019-11-20 08:51:15 +0100

    ejercicio 123 hechos

---
+++ laravel_examen_eval1/laravel_examen_eval1/database/migrations/2019_11_20_074420_create_table_shops.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableShops extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table_shops', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 20);
+            $table->longText('description');
+            $table->string('password', 255);
+            $table->string('email', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_shops');
+    }
+}
--- laravel_examen_eval1/laravel_examen_eval1/resources/views/git.blade.php
+++ laravel_examen_eval1/laravel_examen_eval1/resources/views/git.blade.php
@@ -4,7 +4,17 @@
 
 @section('content')
 
-  Introduce el resultado del ejercicio de git (ejercicio 2).
+1. ¿Qué deberías realizar para fusionar esta rama con la rama master? Indica los comandos necesarios para ello.
+
+	nos tendriamos que situar en la rama master con "git checkout master" y ahi realizar "git merge tarea1" para que nos realice el merge del contenido de la branch tarea1.
+
+2.Indica en que consiste el modelo vista-controlador. Describe brevemente cada uno de los componentes que lo forman.
+
+	El modelo vista controlador es un metodo de programacion en el cual la comunicacion entre el servidor y el cliente de la aplicacion se realiza mediante los componentes "Modelo", "Controlador" y "vistas".
+
+	- Modelo: El modelo se encarga de recoger los datos del servidor y se los suministra al controlador.
+	- Controlador: El controlador se encarga de llamar al modelo para solicitar datos del servidor y suministrarlos a las vistas o realizar cambios a los mismo.
+	- Vistas: Es la zona del cliente en el cual se listan los datos suministrados por el controlador o se hacen llamadas a los controladores para solicitar datos nuevos o cambios a los datos ya almacenados.
 
 
 @endsection
